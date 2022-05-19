86525ae8e60304c0d4f16dcf129cb92b26c64deb
imcrisanto <74809439+imcrisanto@users.noreply.github.com>
2022-05-19 03:53:49 -0400

    recipes

---
+++ lib/screens/recipe_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class RecipeScreen extends StatelessWidget {
+  const RecipeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Text('Tapaking'),
+        title: const Text('Sales Dashboard for Avire Branch'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Sorry no menu available yet'),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Image.asset('/images/wallpaper-2.png'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
