76e15169e10c43efd7a67eeea6201aa6ff49bdc0
imcrisanto <74809439+imcrisanto@users.noreply.github.com>
2022-11-29 21:53:42 +0800

    updated sales screen

--- lib/screens/sales.dart
+++ lib/screens/sales.dart
@@ -8,11 +8,76 @@ class SalesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(
-      children: [
-        const Text('This is the sales screen'),
-        for (int i = 0; i < 10; i++) Text("$i"),
-      ],
+        child: Padding(
+      padding: const EdgeInsets.all(100.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25.0),
+            child: const Text(
+              'Sales Entries',
+              style: TextStyle(
+                  color: Color(0xffD64123),
+                  fontSize: 54,
+                  fontWeight: FontWeight.w800),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Text(
+                'Transaction ID',
+                style: TextStyle(
+                    color: Color(0xff7D0C0E),
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600),
+              ),
+              const Text(
+                'Category',
+                style: TextStyle(
+                    color: Color(0xff7D0C0E),
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600),
+              ),
+              const Text(
+                'Amount',
+                style: TextStyle(
+                    color: Color(0xff7D0C0E),
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600),
+              ),
+              const Text(
+                'Type',
+                style: TextStyle(
+                    color: Color(0xff7D0C0E),
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600),
+              ),
+              const Text(
+                'Date',
+                style: TextStyle(
+                    color: Color(0xff7D0C0E),
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600),
+              ),
+              const Text(
+                'User',
+                style: TextStyle(
+                    color: Color(0xff7D0C0E),
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+          Divider(
+            color: Colors.redAccent, //color of divider
+            height: 5, //height spacing of divider
+            thickness: 2, //thickness of divier line
+          ),
+        ],
+      ),
     ));
   }
 }
